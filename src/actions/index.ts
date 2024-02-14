17d2213d30b198e42c247385d5332b8dec0724f3
Pedro Torres <pedro.areal.torres@ing.com>
2024-02-14 20:21:14 +0100

    feat: create user

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -20,5 +20,50 @@ export async function deleteUser(id: number) {
     where: { id },
   });
 
+  revalidatePath('/');
+  redirect('/');
+}
+
+export async function createUser(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Validate user input
+    const name = formData.get('name') as string;
+    const role = formData.get('role') as string;
+
+    if (typeof name !== 'string' || name.length < 3) {
+      return {
+        message: 'Name must be longer',
+      };
+    }
+
+    if (typeof role !== 'string' || role.length < 3) {
+      return {
+        message: 'Role must be longer',
+      };
+    }
+
+    // Create on the database
+    const user = await db.user.create({
+      data: {
+        name,
+        role,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { message: err.message };
+    } else {
+      return {
+        message: 'Something went wrong',
+      };
+    }
+  }
+
+  // Refresh data stored on cache
+  revalidatePath('/');
+  // Redirect back to homepage
   redirect('/');
 }
